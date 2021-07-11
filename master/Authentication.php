7098df81ae1e763715647be757bbf97f792dda46
Mario Alejandro Benítez Orozco <maalben@gmail.com>
2021-07-11 09:53:52 -0500

    Prueba de comentario...15

--- master/Authentication.php
+++ master/Authentication.php
@@ -49,7 +49,7 @@ class Authentication extends Process
         if (isset($array[Sets::WORD_USER]) || isset($array[Sets::WORD_PASSWORD])) {
             $password = $this->encryptData($array[Sets::WORD_PASSWORD]);
             $array = $this->_getUserData($array[Sets::WORD_USER]);
-            return $this->validate_login($password, $array);
+            return $this->validateLogin($password, $array);
         }
         return $response->formatNotCorrect();
     }
@@ -64,7 +64,7 @@ class Authentication extends Process
      *
      * @return mixed
      */
-    private function validate_login(string $password, array $array): array
+    private function validateLogin(string $password, array $array): array
     {
         $response = new Responses();
         if ($array) {
