7ff424c77221adba91136843308a1e56b56e720d
Mario Alejandro Benítez Orozco <maalben@gmail.com>
2021-08-07 23:59:53 -0500

    Se refactoriza clase base model para quitar codigo repetido

--- v1/model/Patients.php
+++ v1/model/Patients.php
@@ -17,7 +17,9 @@ namespace services\v1\model;
 
 use JsonException;
 use services\master\connection\Executor;
+use services\master\libs\Util;
 use services\master\Responses;
+use services\set\Constant;
 use services\v1\General;
 
 require_once '../../master/connection/Executor.php';
@@ -36,17 +38,56 @@ require_once 'IModel.php';
  */
 class Patients implements IModel
 {
+    public Executor  $process;
+    public Responses $response;
+    public Constant  $constant;
+    public General   $general;
 
-    private string $tablePatients = 'pacientes';
-    private string $codeUser = '';
-    private string $identity = '';
-    private string $nameUser = '';
-    private string $address = '';
-    private string $postal = '';
-    private string $gender = '';
+    private string $codeUser  = '';
+    private string $identity  = '';
+    private string $nameUser  = '';
+    private string $address   = '';
+    private string $postal    = '';
+    private string $gender    = '';
     private string $telephone = '';
-    private string $birth = '0000-00-00';
-    private string $email = '';
+    private string $birth     = '0000-00-00';
+    private string $email     = '';
+
+    private bool   $success;
+    private string $contentType;
+    private string $responds;
+    private string $status;
+    private string $dateRequest;
+    private string $clientIp;
+
+    public function __construct()
+    {
+        $util = new Util();
+        $this->process  = new Executor();
+        $this->response = new Responses();
+        $this->constant = new Constant();
+        $this->general = new General();
+
+        $this->success     = true;
+        $this->contentType = 'application/json; charset=UTF-8';
+        $this->responds    = 'HTTP/1.1 200 OK';
+        $this->status      = '200';
+        $this->dateRequest = Date('Y-m-d H:i:s');
+        $this->clientIp    = $util->getIpClient();
+    }
+
+    /**
+     *
+     * Get ip client
+     *
+     * This method get ip client
+     *
+     * @return string
+     */
+    final public function getClientIp(): string
+    {
+        return $this->clientIp;
+    }
 
     /**
      * List
@@ -58,18 +99,15 @@ class Patients implements IModel
      * @return mixed | int
      * @throws JsonException
      */
-    final public function list(int $page = 1):array
+    final public function list(int $page = 1): array
     {
-        $process = new Executor();
-        $initial  = 0 ;
-        $quantity = 100;
+        $initial = 0;
+        $quantity = 200;
         if ($page > 1) {
-            $initial = ($quantity * ($page - 1)) +1 ;
+            $initial = ($quantity * ($page - 1)) + 1;
             $quantity *= $page;
         }
-        $query = "CALL get_all_patients($initial, $quantity)";
-        $registers = $process->getData($query);
-        return ($registers);
+        return $this->process->getData("CALL get_all_patients($initial,$quantity)");
     }
 
     /**
@@ -82,11 +120,84 @@ class Patients implements IModel
      * @return mixed | int
      * @throws JsonException
      */
-    final public function get(int $codes):array
+    final public function get(int $codes): array
     {
-        $process = new Executor();
-        $query = "CALL get_patient($codes)";
-        return $process->getData($query);
+        return $this->process->getData("CALL get_patient($codes)");
+    }
+
+    /**
+     * Get data register
+     *
+     * This method is useful for get data for use to store procedure
+     *
+     * @return array
+     */
+    final public function getData(): array
+    {
+        $id = 0;
+        if ($this->codeUser !== '') {
+            $id = $this->codeUser;
+        }
+        return array(
+            "'$id'",
+            "'$this->identity'",
+            "'$this->nameUser'",
+            "'$this->address'",
+            "'$this->postal'",
+            "'$this->telephone'",
+            "'$this->gender'",
+            "'$this->birth'",
+            "'$this->email'");
+    }
+
+    /**
+     * Get information response
+     *
+     * This method is useful for get data for use to store procedure
+     *
+     * @param string $process message process class
+     * @param string $message message successfully action
+     *
+     * @return array
+     */
+    final public function getInformationResponse(string $process, string $message): array
+    {
+        return [
+            'Success'          => $this->success,
+            'Content-Type'     => $this->contentType,
+            'Respond'          => $this->responds,
+            'Status'           => $this->status,
+            'Date request'     => $this->dateRequest,
+            'Client ip'        => $this->getClientIp(),
+            'Executed process' => $process,
+            'Message'          => $message
+        ];
+    }
+
+    /**
+     * Process BD actions
+     *
+     * This method return result execute query insert in database
+     *
+     * @param string $action execute process in database
+     *
+     * @return string | int | mixed
+     * @throws JsonException
+     */
+    final public function processBdActions(string $action): int
+    {
+        if ($action === 'insert') {
+            $query = "CALL sp_new_patient(". implode(',', $this->getData()) .")";
+        } elseif ($action === 'update') {
+            $query = "CALL sp_update_patient(". implode(',', $this->getData()) .")";
+        } else {
+            $query = "CALL sp_delete_patient('$this->codeUser')";
+        }
+        $respond = $this->process->nonQuery($query);
+        if ($respond) {
+            return $respond;
+        }
+        return 0;
     }
 
     /**
@@ -99,23 +210,15 @@ class Patients implements IModel
      * @return mixed
      * @throws JsonException
      */
-    final public function postValidate(string $json):array
+    final public function postValidate(string $json): array
     {
-        $response = new Responses();
         $array = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
-
-        $this->incorrectFormat(
-            $array['nombre'],
-            $array['dni'],
-            $array['correo']
-        );
+        $this->incorrectFormat($array['nombre'], $array['dni'], $array['correo']);
 
         $this->validateRequired(
             $array['nombre'],
             $array['dni'],
-            $array['correo']
-        );
-        $this->validateAnother(
+            $array['correo'],
             $array['telefono'],
             $array['direccion'],
             $array['codigoPostal'],
@@ -123,16 +226,17 @@ class Patients implements IModel
             $array['fechaNacimiento']
         );
 
-        $respond = $this->insert();
+        $respond = $this->processBdActions('insert');
 
         if ($respond) {
-            $answer = $response->response;
-            $answer['result'] = [
-                'pacienteId' => $respond
-            ];
+            $answer = $this->response->response;
+            $answer['result'] = $this->getInformationResponse(
+                'Insert patient',
+                'Register inserted successfully'
+            );
             return $answer;
         }
-        return $response->internalError();
+        return $this->response->internalError();
     }
 
     /**
@@ -145,40 +249,34 @@ class Patients implements IModel
      * @return mixed
      * @throws JsonException
      */
-    final public function putValidate(string $json):array
+    final public function putValidate(string $json): array
     {
-        $responses = new Responses();
-        $general = new General();
-        $information = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
-
-        $general->validateIdentity($information['id']);
-
-        $this->codeUser = $information['id'];
+        $array = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+        $this->general->validateIdentity($array['id']);
+        $this->codeUser = $array['id'];
 
         $this->validateRequired(
-            $information['nombre'],
-            $information['dni'],
-            $information['correo']
+            $array['nombre'],
+            $array['dni'],
+            $array['correo'],
+            $array['telefono'],
+            $array['direccion'],
+            $array['codigoPostal'],
+            $array['genero'],
+            $array['fechaNacimiento']
         );
 
-        $this->validateAnother(
-            $information['telefono'],
-            $information['direccion'],
-            $information['codigoPostal'],
-            $information['genero'],
-            $information['fechaNacimiento']
-        );
-
-        $respond = $this->update();
+        $respond = $this->processBdActions('update');
         if ($respond) {
-            $answer = $responses->response;
-            $answer['result'] = [
-                'id' => $this->codeUser
-            ];
+            $answer = $this->response->response;
+            $answer['result'] = $this->getInformationResponse(
+                'Update patient',
+                'Register updated successfully'
+            );
             return $answer;
         }
 
-        return $responses->internalError();
+        return $this->response->internalError();
     }
 
     /**
@@ -191,103 +289,22 @@ class Patients implements IModel
      * @return mixed
      * @throws JsonException
      */
-    final public function deleteValidate(string $json):array
+    final public function deleteValidate(string $json): array
     {
-        $responses = new Responses();
-        $general = new General();
         $information = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
-
-        $general->validateIdentity($information['id']);
+        $this->general->validateIdentity($information['id']);
         $this->codeUser = $information['id'];
 
-        $respond = $this->delete();
+        $respond = $this->processBdActions('delete');
         if ($respond) {
-            $answer = $responses->response;
-            $answer['result'] = [
-                'id' => $this->codeUser
-            ];
+            $answer = $this->response->response;
+            $answer['result'] = $this->getInformationResponse(
+                'Delete patient',
+                'Register deleted successfully'
+            );
             return $answer;
         }
-        return $responses->internalError();
-    }
-
-    /**
-     * Insert register
-     *
-     * This method return result execute query insert in database
-     *
-     * @return string | int | mixed
-     * @throws JsonException
-     */
-    final public function insert():int
-    {
-        $process = new Executor();
-        $query = 'INSERT INTO ' . $this->tablePatients . "
-        (
-        DNI, Nombre, Direccion,
-        CodigoPostal, Telefono, Genero,
-        FechaNacimiento, Correo
-        )
-        values
-        (
-        '$this->identity', '$this->nameUser', '$this->address',
-        '$this->postal', '$this->telephone', '$this->gender',
-        '$this->birth', '$this->email')";
-        $respond = $process->nonQueryId($query);
-        if ($respond) {
-            return $respond;
-        }
-            return 0;
-    }
-
-    /**
-     * Update
-     *
-     * This method return result execute query update
-     *
-     * @return string | int | mixed
-     * @throws JsonException
-     */
-    final public function update():int
-    {
-        $process = new Executor();
-        $query = 'UPDATE ' . $this->tablePatients . "
-        SET 
-        Nombre ='" . $this->nameUser . "',
-        Direccion = '" . $this->address . "', 
-        DNI = '" . $this->identity . "', 
-        CodigoPostal = '" . $this->postal . "', 
-        Telefono = '" . $this->telephone . "', 
-        Genero = '" . $this->gender . "', 
-        FechaNacimiento = '" . $this->birth . "', 
-        Correo = '" . $this->email . "' 
-        WHERE PacienteId = '" . $this->codeUser . "'";
-        $respond = $process->nonQuery($query);
-        if ($respond >= 1) {
-            return $respond;
-        }
-            return 0;
-    }
-
-    /**
-     * Delete
-     *
-     * This method return result execute query delete
-     *
-     * @return string | int | mixed
-     * @throws JsonException
-     */
-    final public function delete():int
-    {
-        $process = new Executor();
-        $query = 'DELETE FROM ' . $this->tablePatients . " 
-        WHERE 
-        PacienteId= '" . $this->codeUser . "'";
-        $respond = $process->nonQuery($query);
-        if ($respond >= 1) {
-            return $respond;
-        }
-        return 0;
+        return $this->response->internalError();
     }
 
     /**
@@ -295,52 +312,46 @@ class Patients implements IModel
      *
      * This method return result execute query for save patient
      *
-     * @param string $name     name user
+     * @param string $name name user
      * @param string $identity identity user
-     * @param string $email    postal user
+     * @param string $email postal user
+     * @param string $telephone telephone user
+     * @param string $address address user
+     * @param string $postal postal user
+     * @param string $gender gender user
+     * @param string $birth birth date user
      *
      * @return string | int | mixed
      */
     final public function validateRequired(
         string $name,
         string $identity,
-        string $email
-    ):bool {
-        if (isset($name, $identity, $email)) {
-            $this->nameUser = $name;
-            $this->identity = $identity;
-            $this->email = $email;
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Validate field
-     *
-     * This method return result execute query for save patient
-     *
-     * @param string $telephone telephone user
-     * @param string $address   address user
-     * @param string $postal    postal user
-     * @param string $gender    gender user
-     * @param string $birth     birth date user
-     *
-     * @return string | int | mixed
-     */
-    final public function validateAnother(
+        string $email,
         string $telephone,
         string $address,
         string $postal,
         string $gender,
         string $birth
-    ):bool {
-        if (isset($telephone, $address, $postal, $gender, $birth)) {
+    ): bool {
+        if (isset(
+            $name,
+            $identity,
+            $email,
+            $telephone,
+            $address,
+            $postal,
+            $gender,
+            $birth
+        )
+        ) {
+            $this->nameUser  = $name;
+            $this->identity  = $identity;
+            $this->email     = $email;
             $this->telephone = $telephone;
-            $this->address = $address;
-            $this->postal = $postal;
-            $this->gender = $gender;
-            $this->birth = $birth;
+            $this->address   = $address;
+            $this->postal    = $postal;
+            $this->gender    = $gender;
+            $this->birth     = $birth;
             return true;
         }
         return false;
@@ -353,7 +364,7 @@ class Patients implements IModel
      *
      * @param string $nameUser name user
      * @param string $identity id user
-     * @param string $email    email user
+     * @param string $email email user
      *
      * @return string | int | mixed
      */
@@ -361,10 +372,14 @@ class Patients implements IModel
         string $nameUser,
         string $identity,
         string $email
-    ):string {
-        $response = new Responses();
-        if (!isset($nameUser, $identity, $email)) {
-            return $response->formatNotCorrect();
+    ): string {
+        if (!isset(
+            $nameUser,
+            $identity,
+            $email
+        )
+        ) {
+            return $this->response->formatNotCorrect();
         }
         return '';
     }
